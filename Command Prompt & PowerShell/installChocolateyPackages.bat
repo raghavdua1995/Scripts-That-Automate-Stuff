415886569663051e42dc124986d339518a91608c
Raghav Dua <raghav.dua@widely.io>
2018-09-09 12:03:40 +0530

    added Postman to instalChocolateyPackages.bat

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,3 +1,3 @@
-choco install git.install tortoisegit  winscp.install putty.install vlc youtube-dl ccleaner dashlane vscode kindle wireshark anki teamviewer nodejs -Y
+choco install git.install tortoisegit  winscp.install putty.install vlc youtube-dl ccleaner dashlane vscode kindle wireshark anki teamviewer nodejs postman -Y
 
 choco install google-drive-file-stream gsuite-sync-outlook -Y
