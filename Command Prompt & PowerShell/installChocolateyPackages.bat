54e20739d0de7653386f6229c94ec268c7276456
Raghav Dua <raghav.dua@widely.io>
2018-10-18 15:58:36 +0530

    Updated Chocolatey packages list

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,3 +1,1 @@
-choco install git.install tortoisegit  winscp.install putty.install vlc youtube-dl ccleaner dashlane vscode kindle wireshark anki teamviewer nodejs postman firefox brave -Y
-
-choco install google-drive-file-stream gsuite-sync-outlook -Y
+choco install git.install tortoisegit  winscp.install putty.install vlc youtube-dl ccleaner dashlane vscode kindle wireshark anki teamviewer nodejs postman googlechrome firefox thunderbird libreoffice-fresh -Y
