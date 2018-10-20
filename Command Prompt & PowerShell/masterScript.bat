8a3c5c690d23006817bd60913b79e8136e12d8e4
Raghav Dua <me@raghavdua.in>
2018-10-20 19:27:59 +0530

    Added ability to create 'Source Code' folder

--- Command Prompt & PowerShell/masterScript.bat
+++ Command Prompt & PowerShell/masterScript.bat
@@ -9,3 +9,7 @@ call registry-tweaks.reg
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -File systemSettings.ps1
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -File configureServices.ps1
+
+mkdir 'C:\Users\me\Source Code'
+mkdir 'C:\Users\me\Source Code\Personal'
+mkdir 'C:\Users\me\Source Code\Widely'
