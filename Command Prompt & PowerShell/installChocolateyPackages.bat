9ed3975d359e749bd04e40c94c876d07b1789918
Raghav Dua <raghav.dua@widely.io>
2018-11-16 15:15:54 +0530

    Added Mikitex And SendToKindle to installChocolateyPackages.bat

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,1 +1,1 @@
-choco install git.install tortoisegit winscp.install putty.install vlc youtube-dl ccleaner bitwarden vscode kindle wireshark anki teamviewer nodejs insomnia-rest-api-client googlechrome firefox thunderbird libreoffice-still androidstudio zoom -Y
+choco install git.install tortoisegit winscp.install putty.install vlc youtube-dl ccleaner bitwarden vscode kindle miktex.install wireshark anki teamviewer nodejs insomnia-rest-api-client googlechrome firefox thunderbird libreoffice-still androidstudio sendtokindle zoom -Y
