c4d84f36cbd569b3094ab0e217f17cc683f7cfe9
Raghav Dua <raghav.dua@widely.io>
2019-05-10 16:54:58 +0530

    Removed Spotify from installChocolateyPackages.bat

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,1 +1,1 @@
-choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden vscode kindle miktex.install wireshark teamviewer nodejs-lts insomnia-rest-api-client googlechrome sendtokindle zoom spotify -Y
+choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden vscode kindle miktex.install wireshark teamviewer nodejs-lts insomnia-rest-api-client googlechrome sendtokindle zoom -Y
