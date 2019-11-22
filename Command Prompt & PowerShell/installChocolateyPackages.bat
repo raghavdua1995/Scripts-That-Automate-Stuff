334c4e7074964d4105ce8b394252386e595ff893
Raghav Dua <raghavdua.cse2018@gmail.com>
2019-11-22 13:51:10 +0530

    Added more Chocolatey packages Install Node.js through NVM

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,1 +1,5 @@
-choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden vscode kindle teamviewer postman googlechrome -Y
+choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden vscode kindle teamviewer postman googlechrome firefox choco install python anaconda3 yarn nvm.portable -Y
+
+ C:\ProgramData\nvm\nvm.exe install latest
+C:\ProgramData\nvm\nvm.exe install 12.13.1
+ C:\ProgramData\nvm\nvm.exe use 12.13.1
