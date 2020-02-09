b9f5936d4bbfee854005574dd2dc8de5d84f449c
Raghav Dua <raghav.cse2018@gmail.com>
2020-02-09 23:24:17 +0530

    Removed Google Chrome from installChocolateyPackages.bat

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,4 +1,4 @@
-choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden evernote vscode kindle teamviewer postman googlechrome firefox python anaconda3 yarn nvm.portable texstudio -Y
+choco install git.install tortoisegit vlc youtube-dl ccleaner bitwarden evernote vscode kindle teamviewer postman firefox python anaconda3 yarn nvm.portable texstudio -Y
 
 C:\ProgramData\nvm\nvm.exe install latest
 C:\ProgramData\nvm\nvm.exe install 12.13.1
