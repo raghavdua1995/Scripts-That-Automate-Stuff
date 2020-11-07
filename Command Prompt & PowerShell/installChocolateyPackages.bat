78f144aeb446058ed3faeb93a01550e806c6a5cd
Raghav Dua <raghav.cse2018@gmail.com>
2020-11-07 16:34:31 +0530

    Package changes and updates in installChocolateyPackages.bat

--- Command Prompt & PowerShell/installChocolateyPackages.bat
+++ Command Prompt & PowerShell/installChocolateyPackages.bat
@@ -1,5 +1,5 @@
-choco install git.install tortoisegit vlc youtube-dl bitwarden vscode kindle sendtokindle teamviewer postman firefox python yarn nvm.portable texstudio powertoys google-backup-and-sync -Y
+choco install git.install tortoisegit vlc youtube-dl bitwarden vscode kindle sendtokindle teamviewer postman firefox python3 yarn nvm.portable texstudio powertoys google-backup-and-sync -Y
 
 C:\ProgramData\nvm\nvm.exe install latest
-C:\ProgramData\nvm\nvm.exe install 12.13.1
-C:\ProgramData\nvm\nvm.exe use 12.13.1
+C:\ProgramData\nvm\nvm.exe install 14.15.0
+C:\ProgramData\nvm\nvm.exe use 15.1.0
